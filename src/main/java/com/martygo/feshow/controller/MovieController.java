ea5b1bf194cdac230d77904583685038b5a96672
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-16 20:03:14 +0100

    ✨ Add enpoint for create movie

--- src/main/java/com/martygo/feshow/controller/MovieController.java
+++ src/main/java/com/martygo/feshow/controller/MovieController.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import com.martygo.feshow.domain.Movie;
 import com.martygo.feshow.services.MovieService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -17,8 +21,9 @@ public class MovieController {
     private MovieService movieService;
 
     @PostMapping
-    public void save(@RequestBody Movie movie) {
-        movieService.create(movie);
+    public ResponseEntity<Movie> save(@Valid @RequestBody Movie movie) {
+        Movie savedMovie = movieService.create(movie);
+
+        return new ResponseEntity<Movie>(savedMovie, HttpStatus.CREATED);
     }
-    
 }
