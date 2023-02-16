c1474281d7f52a5c08cad02f4cdecd32dae14c7d
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-16 05:33:42 +0100

    🎉 Begin project

---
+++ src/main/java/com/martygo/feshow/FeshowApplication.java
@@ -0,0 +1,13 @@
+package com.martygo.feshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martygo/feshow/FeshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martygo.feshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
