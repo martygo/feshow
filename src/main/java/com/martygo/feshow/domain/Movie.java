b7f03d8d9695a2d5bcb57d247b19960306df741c
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-16 11:07:06 +0100

    ✨ Add Movie model

---
+++ src/main/java/com/martygo/feshow/domain/Movie.java
@@ -0,0 +1,42 @@
+package com.martygo.feshow.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode
+
+@Table(name = "movies")
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "Please provide a title")
+    @Size(min = 3, message = "Title must be at least 3 characters long")
+    private String title;
+
+    private String description;
+
+    @NotEmpty(message = "Please provide a link to the poster")
+    private String poster;
+
+    private String trailer;
+    private String genre;
+
+    @NotEmpty(message = "Please fill this field")
+    private Boolean isRelease;
+    private int year;
+}
