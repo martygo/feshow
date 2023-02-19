3bfc66cead041fe5a32157974d30e8f4251f4e2c
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-19 08:27:04 +0100

    🚧 In drafted

--- src/main/java/com/martygo/feshow/controller/MovieController.java
+++ src/main/java/com/martygo/feshow/controller/MovieController.java
@@ -6,7 +6,10 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
@@ -34,5 +37,25 @@ public class MovieController {
         log.info("Saving movie: {}", movie);
 
         return new ResponseEntity<Movie>(movieService.create(movie), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return new ResponseEntity<String>("Get all...", HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById() {
+        return new ResponseEntity<String>("Getting one....", HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update() {
+        return new ResponseEntity<String>("Update...", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete() {
+        return new ResponseEntity<String>("Delete...", HttpStatus.OK);
     }
 }
