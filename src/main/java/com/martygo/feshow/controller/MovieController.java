0705b21efa01da6bfa49df2c92a08e1ebfc8c9aa
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-26 22:41:22 +0100

    ✨ Added new endpoints

--- src/main/java/com/martygo/feshow/controller/MovieController.java
+++ src/main/java/com/martygo/feshow/controller/MovieController.java
@@ -4,19 +4,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
+
+import com.martygo.feshow.domain.Movie;
+import com.martygo.feshow.services.MovieService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-
-import com.martygo.feshow.domain.Movie;
-import com.martygo.feshow.services.MovieService;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -35,27 +37,65 @@ public class MovieController {
         movie.setUpdatedAt(LocalDateTime.now(ZoneId.of("UTC")));
 
         log.info("Saving movie: {}", movie);
-
         return new ResponseEntity<Movie>(movieService.create(movie), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<String> get() {
-        return new ResponseEntity<String>("Get all...", HttpStatus.OK);
+    public ResponseEntity<Iterable<Movie>> get() {
+        log.info("Getting all movies");
+        return new ResponseEntity<Iterable<Movie>>(movieService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getById() {
-        return new ResponseEntity<String>("Getting one....", HttpStatus.OK);
+    public ResponseEntity<Object> getById(@PathVariable("id") Long id) {
+        log.info("Getting movie by id: {}", id);
+
+        Optional<Movie> movie = movieService.findById(id);
+
+        if (!movie.isPresent()) {
+            return new ResponseEntity<Object>("Movie not found." , HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Object>(movie.get(), HttpStatus.OK);
     }
 
-    @PutMapping
-    public ResponseEntity<String> update() {
-        return new ResponseEntity<String>("Update...", HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> update(@PathVariable(value = "id") Long id, @RequestBody @Valid Movie movie) {
+        log.info("Updating movie by id: {}", id);
+
+        Optional<Movie> movieOptional = movieService.findById(id);
+
+        if (!movieOptional.isPresent()) {
+            return new ResponseEntity<Movie>(movie, HttpStatus.NOT_FOUND);
+        }
+
+        if(movieOptional.get().getId() != id) {
+            return new ResponseEntity<Movie>(movie, HttpStatus.NOT_FOUND);
+        }
+
+        movie.setId(movieOptional.get().getId());
+        movie.setCreatedAt(movieOptional.get().getCreatedAt());
+        movie.setUpdatedAt(LocalDateTime.now(ZoneId.of("UTC")));
+
+        return new ResponseEntity<Movie>(movieService.create(movie), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete() {
-        return new ResponseEntity<String>("Delete...", HttpStatus.OK);
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+        log.info("Deleting movie by id: {}", id);
+
+        Optional<Movie> movie = movieService.findById(id);
+
+        if (!movie.isPresent()) {
+            return new ResponseEntity<String>("Not found." , HttpStatus.NOT_FOUND);
+        }
+
+        if(movie.get().getId() != id) {
+            return new ResponseEntity<String>("Not found." , HttpStatus.NOT_FOUND);
+        }
+
+        movieService.delete(movie.get());
+
+        return new ResponseEntity<String>("Movie deleted successfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/martygo/feshow/services/MovieService.java
+++ src/main/java/com/martygo/feshow/services/MovieService.java
@@ -1,10 +1,13 @@
 package com.martygo.feshow.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import com.martygo.feshow.domain.Movie;
 import com.martygo.feshow.repo.MovieRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class MovieService {
@@ -12,7 +15,19 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
+    public Iterable<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id) {
+        return movieRepository.findById(id);
+    }
+
     public Movie create(Movie movie) {
         return movieRepository.save(movie);
+    }
+
+    public void delete(Movie movie) {
+        movieRepository.delete(movie);
     }
 }
