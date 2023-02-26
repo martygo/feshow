d5e5a09d45d0b67ca7d694d9a5c9eb806d6f334a
Martins Gouveia <gouveiamartins6@gmail.com>
2023-02-26 22:52:31 +0100

    🎨 Removed validation annotations

--- src/main/java/com/martygo/feshow/domain/Movie.java
+++ src/main/java/com/martygo/feshow/domain/Movie.java
@@ -7,8 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -26,15 +24,9 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotEmpty(message = "Please provide a title")
-    @Size(min = 3, message = "Title must be at least 3 characters long")
     private String title;
-
     private String description;
-
-    @NotEmpty(message = "Please provide a link to the poster")
     private String poster;
-
     private String trailer;
     private String genre;
     private Boolean isRelease;
