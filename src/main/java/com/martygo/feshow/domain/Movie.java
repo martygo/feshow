84de9edc571213af652d2ae4f64c7e484da5ec49
Martins Gouveia <gouveiamartins6@gmail.com>
2023-03-13 11:30:05 +0100

    ✨ Add builder pattern

--- src/main/java/com/martygo/feshow/domain/Movie.java
+++ src/main/java/com/martygo/feshow/domain/Movie.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @EqualsAndHashCode
+@Builder
 
 @Table(name = "movies")
 @Entity
