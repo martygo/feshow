2dbcbdd0726d9331dad1d4c52db4da933903c637
Martins Gouveia <gouveiamartins6@gmail.com>
2023-03-13 11:30:42 +0100

    ✨ Add commandliner runner to populate data

--- src/main/java/com/martygo/feshow/Main.java
+++ src/main/java/com/martygo/feshow/Main.java
@@ -1,10 +1,33 @@
 package com.martygo.feshow;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.martygo.feshow.domain.Movie;
+import com.martygo.feshow.repo.MovieRepository;
 
 @SpringBootApplication
 public class Main {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired MovieRepository movieRepository) {
+		return args -> {
+			Movie movie = Movie.builder()
+			.title("The Matrix")
+			.description("movie")
+			.poster("poster")
+			.trailer("poster")
+			.genre("action")
+			.isRelease(true)
+			.year(1999)
+			.build();
+
+			movieRepository.save(movie);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
