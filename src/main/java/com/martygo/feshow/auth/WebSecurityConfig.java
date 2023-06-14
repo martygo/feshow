ea2de0611b7a88a1bebf88ce02cbed54517d317b
Martins Gouveia <gouveiamartins6@gmail.com>
2023-06-14 15:53:26 -0300

    ✨ Add authentication in memory

--- src/main/java/com/martygo/feshow/auth/WebSecurityConfig.java
+++ src/main/java/com/martygo/feshow/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.martygo.feshow.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,30 +10,43 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         String cryptPassword = passwordEncoder.encode("1234");
 
         UserDetails admin = User.withUsername("marty")
-            .password(cryptPassword)
-            .roles("ADMIN")
-            .build();
+                .password(cryptPassword)
+                .roles("ADMIN")
+                .build();
 
         UserDetails user = User.withUsername("martins")
-            .password(cryptPassword)
-            .roles("USER")
-            .build();
+                .password(cryptPassword)
+                .roles("USER")
+                .build();
 
         return new InMemoryUserDetailsManager(admin, user);
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers("/welcome").permitAll()
+                .requestMatchers("/categories/**").hasRole("ADMIN")
+                .requestMatchers("/movies/**").hasRole("USER")
+                .anyRequest().authenticated())
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
